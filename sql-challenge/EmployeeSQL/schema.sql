fe7572fbbc89c821a8dc5951819b0d7988c6b607
Costilrt <cost_rol@hotmail.com>
2022-06-01 20:50:07 -0400

    employeeSQL

---
+++ sql-challenge/EmployeeSQL/schema.sql
@@ -0,0 +1,52 @@
+-- Drop Tables if needed
+DROP TABLE Department_Managers;
+DROP TABLE Department_Employees;
+DROP TABLE Departments;
+DROP TABLE Salaries;
+DROP TABLE Employees;
+DROP TABLE Titles;
+
+
+
+CREATE TABLE Titles (
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR NOT NULL
+);
+
+CREATE TABLE Employees (
+	emp_no INT NOT NULL PRIMARY KEY,
+	emp_title VARCHAR(5) NOT NULL,
+	FOREIGN KEY (emp_title) REFERENCES Titles(title_id),
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL
+);
+
+CREATE TABLE Salaries (
+	emp_no INT NOT NULL PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	salary INT NOT NULL
+);
+
+CREATE TABLE Departments (
+	dept_no VARCHAR(4) NOT NULL PRIMARY KEY,
+	dept_name VARCHAR NOT NULL
+);
+
+CREATE TABLE Department_Employees (
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE Department_Managers (
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
